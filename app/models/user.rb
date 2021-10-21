de98fe7fbce7a724735dd17679e48d9ce4ca9b58
eric <contreras.eric18@gmail.com>
2021-10-21 15:48:37 -0400

    added post resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true
+    validates :username, presence: true, uniqueness: true
+    has_many :posts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :posts
     resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
