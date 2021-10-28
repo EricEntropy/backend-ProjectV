d29fd4a51a68ddff4e252dbf7211ad58d4d8aba4
eric <contreras.eric18@gmail.com>
2021-10-28 17:08:21 -0400

    added more seeds and changed serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
 
-user = User.create(
+user1 = User.create(
     username: "eric",
     password: "password")
 
-user.posts.create(
+user1.posts.create(
     title: "hello",
     content: "world" 
+)
+
+
+user2 = User.create(
+    username: "king",
+    password: "password")
+
+user2.posts.create(
+    title: "hello",
+    content: "moon" 
+)
+
+
+user3 = User.create(
+    username: "jupiter",
+    password: "password")
+
+user3.posts.create(
+    title: "hello",
+    content: "sun" 
 )
